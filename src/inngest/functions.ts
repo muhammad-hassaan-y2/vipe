99826967cf2d58eaae815344254bd61dbbef8a11
muhammad-hassaan-y2 <mr.hassaan50@gmail.com>
2025-07-05 18:39:11 +0500

    Sand Boxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,19 @@
+import { Sandbox } from "@e2b/code-interpreter";
+
+import { getSandbox } from "./utils";
+
 import { inngest } from "./client";
 import { openai, createAgent} from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+
+     const sandboxId = await step.run("get-sandbox-id", async () => {
+       const sandbox = await Sandbox.create("vibe-nextjs-test234355");
+       return sandbox.sandboxId;
+     })
 
    const summarizer = createAgent({
     name: "summarizer",
@@ -16,7 +25,14 @@ export const helloWorld = inngest.createFunction(
      `Summarizer the following text: ${event.data.value}`
    );
    console.log(output)
-   
-    return { output};
+
+   const sandboxUrl = await step.run("get-sandbox-url", async () => {
+    const sandbox = await getSandbox(sandboxId);
+    const host = sandbox.getHost(3000);
+    return `http://${host}`;
+   }) 
+
+    return { output, sandboxUrl };
+
   },
 );
