a787b30d8829e0551fbc04b0afed3cfe850899ba
JIA <javaria.y2b@gmail.com>
2025-07-07 08:36:28 +0500

    09: projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,43 @@
 "use client";
 
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
-  const [value, setValue] = useState("trpc.messages");
+  const router = useRouter();
+  const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions())
-  const createMessage = useMutation(
-    trpc.messages.create.mutationOptions({
-      onSuccess: () => {
-        toast.success("MESSAGE CREATED!");
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onError: (error) => {
+        toast.error(error.message);
+      },
+      onSuccess: (data) => {
+        router.push(`/projects/${data.id}`);
       },
     })
   );
 
   return (
-    <div className="p-4 max-w-7xl mx-auto">
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-centr">
+
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
-        disabled={createMessage.isPending}
-        onClick={() => createMessage.mutate({ value: value })}
+        disabled={createProject.isPending}
+        onClick={() => createProject.mutate({ value: value })}
       >
-        Invoke Background Job
+        Submit
       </Button>
-      {JSON.stringify(messages, null, 2)}
+      
+      </div>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,10 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
-import { inngest } from '@/inngest/client';
+import { createTRPCRouter } from '../init';
 import { messagesRouter } from '@/modules/messages/server/procedures';
+import { projectsRouter } from '@/modules/projects/server/procedures';
 
 export const appRouter = createTRPCRouter({
-messages: messagesRouter
+messages: messagesRouter,
+projects: projectsRouter
 })
 
  
