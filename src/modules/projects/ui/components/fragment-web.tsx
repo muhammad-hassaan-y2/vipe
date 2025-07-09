1d9301f54e809de105099a1375abc6d52328468a
JIA <javaria.y2b@gmail.com>
2025-07-09 17:30:49 +0500

    13: code-view

--- src/modules/projects/ui/components/fragment-web.tsx
+++ src/modules/projects/ui/components/fragment-web.tsx
@@ -10,18 +10,18 @@ interface Props {
 }
 
 export function FragmentWeb({ data }: Props) {
-    const [copied, setCopied] = useState(false);
-const [fragmentKey, setFragmentKey] = useState(0);
+  const [copied, setCopied] = useState(false);
+  const [fragmentKey, setFragmentKey] = useState(0);
 
-const onRefresh = () => {
-  setFragmentKey((prev) => prev + 1);
-};
+  const onRefresh = () => {
+    setFragmentKey((prev) => prev + 1);
+  };
 
-const handleCopy = () => {
-  navigator.clipboard.writeText(data.sandboxUrl);
-  setCopied(true);
-  setTimeout(() => setCopied(false), 2000);
-};
+  const handleCopy = () => {
+    navigator.clipboard.writeText(data.sandboxUrl);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
 
   return (
     <div className="flex flex-col w-full h-full">
@@ -29,36 +29,35 @@ const handleCopy = () => {
         <Button size="sm" variant="outline" onClick={onRefresh}>
           <RefreshCcwIcon />
         </Button>
-                <Hint text="Click to copy" side="bottom">
-
-        <Button
-          size="sm"
-          variant="outline"
-          disabled={!data.sandboxUrl || copied}
-          onClick={handleCopy}
-          className="flex-1 justify-start text-start font-normal"
-        >
-          <span className="truncate">{data.sandboxUrl}</span>
-        </Button>
+        <Hint text="Click to copy" side="bottom">
+          <Button
+            size="sm"
+            variant="outline"
+            disabled={!data.sandboxUrl || copied}
+            onClick={handleCopy}
+            className="flex-1 justify-start text-start font-normal"
+          >
+            <span className="truncate">{data.sandboxUrl}</span>
+          </Button>
         </Hint>
 
         <Hint text="Open in new tab" side="bottom" align="start">
-        <Button
-          size="sm"
-          disabled={!data.sandboxUrl}
-          variant="outline"
-          onClick={() => {
-            if (!data.sandboxUrl) return;
-            window.open(data.sandboxUrl, "_blank");
-          }}
-        >
-          <ExternalLinkIcon />
-        </Button>
+          <Button
+            size="sm"
+            disabled={!data.sandboxUrl}
+            variant="outline"
+            onClick={() => {
+              if (!data.sandboxUrl) return;
+              window.open(data.sandboxUrl, "_blank");
+            }}
+          >
+            <ExternalLinkIcon />
+          </Button>
         </Hint>
       </div>
 
       <iframe
-      key={fragmentKey}
+        key={fragmentKey}
         className="h-full w-full"
         sandbox="allow-forms allow-scripts allow-same-origin"
         loading="lazy"
--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -11,14 +11,20 @@ import { Fragment } from "@/generated/prisma";
 import { ProjectHeader } from "../components/project-header";
 import { FragmentWeb } from "../components/fragment-web";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { CodeIcon, CrownIcon, EyeIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { CodeView } from "@/components/code-view";
+import { FileExplorer } from "@/components/file-explorer";
+
 interface Props {
   projectId: string;
 }
 
 export const ProjectView = ({ projectId }: Props) => {
-
   const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
-
+  const [tabState, setTabState] = useState<"preview" | "code">("preview");
 
   return (
     <div className="h-screen">
@@ -28,18 +34,51 @@ export const ProjectView = ({ projectId }: Props) => {
           minSize={20}
           className="flex flex-col min-h-0"
         >
-
           <Suspense fallback={<p>Loading messages...</p>}>
             <ProjectHeader projectId={projectId} />
-         <MessagesContainer projectId={projectId}
-         activeFragment={activeFragment}
-          setActiveFragment={setActiveFragment}
-         />
+            <MessagesContainer
+              projectId={projectId}
+              activeFragment={activeFragment}
+              setActiveFragment={setActiveFragment}
+            />
           </Suspense>
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={65} minSize={50}>
-          {!!activeFragment && <FragmentWeb data={activeFragment}/>}
+          <Tabs
+            className="h-full gap-y-0"
+            defaultValue="preview"
+            value={tabState}
+            onValueChange={(value) => setTabState(value as "preview" | "code")}
+          >
+            <div className="w-full flex items-center p-2 border-b gap-x-2">
+              <TabsList className="h-8 p-0 border rounded-md">
+                <TabsTrigger value="preview" className="rounded-md">
+                  <EyeIcon /> <span>Demo</span>
+                </TabsTrigger>
+                <TabsTrigger value="code" className="rounded-md">
+                  <CodeIcon /> <span>Code</span>
+                </TabsTrigger>
+              </TabsList>
+              <div className="ml-auto flex items-center gap-x-2">
+                <Button asChild size="sm" variant="default">
+                  <Link href="/pricing">
+                    <CrownIcon /> Upgrade
+                  </Link>
+                </Button>
+              </div>
+            </div>
+            <TabsContent value="preview">
+              {!!activeFragment && <FragmentWeb data={activeFragment} />}
+            </TabsContent>
+            <TabsContent value="code" className="min-h-0">
+              {!!activeFragment?.files && (
+                <FileExplorer
+                  files={activeFragment.files as { [path: string]: string }}
+                />
+              )}
+            </TabsContent>{" "}
+          </Tabs>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
