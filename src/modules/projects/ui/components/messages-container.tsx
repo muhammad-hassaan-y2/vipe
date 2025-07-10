b51d85a66256857954cce6bf481596b536f3715f
JIA <javaria.y2b@gmail.com>
2025-07-10 15:53:38 +0500

    14: home page

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -9,7 +9,7 @@ import { MessageLoading } from "./message-loading";
 interface Props {
   projectId: string;
   activeFragment?: Fragment | null;
-  setActiveFragment?: (fragment: Fragment | null) => void;
+  setActiveFragment: (fragment: Fragment | null) => void;
 }
 
 export const MessagesContainer = ({
@@ -19,6 +19,7 @@ export const MessagesContainer = ({
 }: Props) => {
   const bottomRef = useRef<HTMLDivElement>(null);
   const trpc = useTRPC();
+  const lastAssistantMessageIdRef = useRef<string | null>(null);
 
   const { data: messages } = useSuspenseQuery(
     trpc.messages.getMany.queryOptions(
@@ -31,15 +32,19 @@ export const MessagesContainer = ({
     )
   );
 
-  // useEffect(() => {
-  //   const lastAssistantMessageWithFragment = messages.findLast(
-  //     (message) => message.role === "ASSISTANT" && !!message.fragment
-  //   );
+  useEffect(() => {
+    const lastAssistantMessage = messages.findLast(
+      (messages) => messages.role === "ASSISTANT"
+    );
 
-  //   if (lastAssistantMessageWithFragment) {
-  //     setActiveFragment?.(lastAssistantMessageWithFragment.fragment);
-  //   }
-  // }, [messages, setActiveFragment]);
+    if (
+      lastAssistantMessage?.fragment &&
+      lastAssistantMessage.id !== lastAssistantMessageIdRef.current
+    ) {
+      setActiveFragment(lastAssistantMessage.fragment);
+      lastAssistantMessageIdRef.current = lastAssistantMessage.id;
+    }
+  }, [messages, setActiveFragment]); 
 
   useEffect(() => {
     bottomRef.current?.scrollIntoView();
